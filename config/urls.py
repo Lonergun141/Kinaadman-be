20e1a9297aa881c0965c1e31ab1d86008955aef8
Clyde Gevero <gevero530@gmail.com>
2026-02-21 22:56:59 +0800

    feat: Set up initial URL configuration, custom admin settings, and a project landing page.

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from config.api import api
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 admin.site.site_header = "Kinaadman Super Admin"
 admin.site.site_title = "Kinaadman Dashboard"
@@ -16,10 +19,8 @@ def custom_admin_logout(request):
     return redirect('/admin/login/')
 
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/logout/', custom_admin_logout),
     path('admin/', admin.site.urls),
     path('v1/', api.urls),
